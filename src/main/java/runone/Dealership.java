1c2cc51931c67a97fd0e03eab0b21dfeb0cb533b
olumide-AI <olumidekolawole26@gmail.com>
2025-05-10 15:06:49 -0700

    Dealership class implemetation is complete with constructor, addVehicle methods and getList of all vehivles methods

--- src/main/java/runone/Dealership.java
+++ src/main/java/runone/Dealership.java
@@ -4,9 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Dealership {
+    //Fields
     private String name;
     private String address;
     private  String phone;
+    private List<Vehicle> inventory;
 
+    //Constructor
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = new ArrayList<>();
+    }
 
+    //Methods
+    public void addVehicle (Vehicle vehicle){
+        inventory.add(vehicle);
+    }
+
+    public List<Vehicle> getAllVehicles(){
+         return new ArrayList<>(this.inventory);//Safe: changes affect the copy, not the original
+    }
 }
+
--- src/main/java/runone/DebugMain.java
+++ src/main/java/runone/DebugMain.java
@@ -4,7 +4,16 @@ public class DebugMain {
     public static void main(String[] args) {
 
         //Debug vehicle logic
-        Vehicle vehicle1 = new Vehicle(1,1997,"toyota","camry","sedan","red",100000,2000);
+        Vehicle vehicle1 = new Vehicle(1,1997,"toyota","camry","sedan","red",100000,2000.00);
         System.out.println(vehicle1);
+
+        //Add second  vehicle
+        Vehicle vehicle2 = new Vehicle(123, 2020, "Honda", "Civic", "Sedan", "Blue", 15000, 20000.00);
+
+        //Debug Dealership class logic
+        Dealership dealership1 = new Dealership("Olu Cars", "1230 10th, ST, Oakland, CA", "223-345-2343");
+        dealership1.addVehicle(vehicle1);
+        dealership1.addVehicle(vehicle2);
+        System.out.println(dealership1.getAllVehicles());
     }
 }
