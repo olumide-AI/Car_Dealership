87cfa878fcd69de7f615f67c227c70765ad1db64
olumide-AI <olumidekolawole26@gmail.com>
2025-05-16 11:46:36 -0700

    implemented method for lease contract

--- src/main/java/runone/LeaseContract.java
+++ src/main/java/runone/LeaseContract.java
@@ -1,4 +1,32 @@
 package runone;
 
-public class LeaseContract {
+import java.util.List;
+
+public class LeaseContract extends Contract{
+    private static final double LEASE_FEE = 0.07;
+    private static final double EXPECTED_ENDING_VALUE = 0.50;
+
+    public LeaseContract(String date, String customerName, String customerEmail, Vehicle vehicleSold, List<Vehicle> vehicleList) {
+        super(date, customerName, customerEmail, vehicleSold, vehicleList);
+    }
+    public double getLeaseFee(){
+        return getVehicleSold().getPrice() * LEASE_FEE;
+    }
+
+    public double getExpectedEndingValue(){
+        return getVehicleSold().getPrice() * EXPECTED_ENDING_VALUE;
+    }
+
+    @Override
+    public double getTotalPrice() {
+
+        return getVehicleSold().getPrice() + getLeaseFee();
+    }
+
+    @Override
+    public double getMonthlyPayment(){
+        //total price financed at 4% for 36 month
+        //totalprice * (1+0.04)/36
+        return getTotalPrice() * (1+0.04)/36;
+    }
 }
