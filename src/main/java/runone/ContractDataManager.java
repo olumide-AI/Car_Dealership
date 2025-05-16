4630cc403bebda6c979a887d919f2e8117f4d005
olumide-AI <olumidekolawole26@gmail.com>
2025-05-16 13:33:41 -0700

    saving file using file writer

--- src/main/java/runone/ContractDataManager.java
+++ src/main/java/runone/ContractDataManager.java
@@ -1,4 +1,17 @@
 package runone;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class ContractDataManager {
+    private static final String FILE_PATH = "contracts.csv";
+    //Writing a file using save contract
+    public static void saveContract(Contract contract){
+        try(FileWriter fileWriter = new FileWriter(FILE_PATH, true)){
+            fileWriter.write(contract.toFileString()+"\n");
+        }
+        catch (IOException e){
+            System.out.println("File writing error " + e.getMessage());
+        }
+    }
 }
